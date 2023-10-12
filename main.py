7a71fd29a084cb13c92e8b46e7011758d1d4fdae
gowoony <147579478+gowoony@users.noreply.github.com>
2023-10-12 15:16:22 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,9 @@ from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 app.mount("/", StaticFiles(directory="public", html = True), name="static")
+
+@app.get("/hello")
+def read_root():
+    return{"Hello":"고운님 어서오세요!"}
+
+app.mount("/", StaticFiles(directory="Public",html =True),name="static")
